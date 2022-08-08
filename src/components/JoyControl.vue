881c03ac27eda751019ba823145d5209033f367a
kcwuttikorn <kcwuttikorn@gmail.com>
2022-08-08 14:50:38 +0700

    #

---
+++ src/components/JoyControl.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="sidebar">
+    <v-container fluid>
+      <v-row class="blue-grey lighten-5">
+        <v-col cols="6">
+          
+            <v-select
+            v-model="selectedMode"
+            :items="modes"
+            :menu-props="{ maxHeight: '400' }"
+            label="Select"
+            dense
+          ></v-select>
+          
+          <v-row>
+            <v-col >
+              <v-btn color="primary" elevation="2" block outlined
+                >Pose estimate</v-btn
+              >
+            </v-col>
+
+            <v-col >
+              <v-btn color="primary" elevation="2" block outlined
+                >Navigate</v-btn
+              >
+            </v-col>
+
+         
+          </v-row>
+        </v-col>
+
+        <v-col cols="6" sm="6">
+          <div id="zone_joystick"></div>
+        </v-col>
+      </v-row>
+      <!-- <v-row>
+        <div id="zone_joystick" ></div>
+      </v-row> -->
+    </v-container>
+  </div>
+</template>
+
+<script>
+import nipplesjs from "nipplejs";
+
+export default {
+  name: "JoyControl",
+  data() {
+    return {
+      selectedMode: "",
+      modes: ["SLAM Mode", "Navigation Mode"],
+    };
+  },
+  mounted() {
+    const manager = nipplesjs.create({
+      zone: document.getElementById("zone_joystick"),
+      mode: "static",
+      position: { left: "87.5%", top: "89%" },
+      color: "blue",
+      threshold: 0.25,
+      fadeTime: 400,
+      size: 120,
+      maxNumberOfNipples: 1,
+    });
+
+    manager.on("start", function (event, nipple) {
+      console.log("Movement start");
+    });
+
+    manager.on("move", function (event, nipple) {
+      console.log("Moving");
+    });
+
+    manager.on("end", function () {
+      console.log("Movement end");
+    });
+  },
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <v-container fluid >
+      <!-- <v-row class="yellow lighten-5" > -->
+      <v-row>
+        <v-col cols="6">
+          <DisplayPose />          
+        </v-col>
+        <v-col cols="6">
+          <SideBar />
+          <JoyControl />
+        </v-col>
+        
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+//import ToolBar from '../components/ToolBar'
+// import ToolBar from '@/components/ToolBar.vue'
+import DisplayPose from "@/components/DisplayPose.vue";
+import SideBar from "@/components/SideBar.vue";
+import JoyControl from "@/components/JoyControl.vue";
+// import DisplayPose from '@/components/DisplayPose.vue'
+
+export default {
+  name: "Home",
+  components: {
+    DisplayPose,
+    SideBar,
+    JoyControl
+},
+};
+</script>
